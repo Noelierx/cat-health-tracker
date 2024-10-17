a7df3a85706778c57ea5dc6af8eb160ea9de95c4
Noëlie <noelieroux11@gmail.com>
2024-10-17 19:35:35 +0200

    modifCypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'cypress';
-import browserify from '@cypress/browserify-preprocessor';
-import cucumber from 'cypress-cucumber-preprocessor'; // Use * as to import correctly
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    specPattern: '**/*.feature',
     setupNodeEvents(on, config) {
-      const options = browserify.defaultOptions;
-      options.typescript = require.resolve('typescript');
-      on('file:preprocessor', cucumber.default()); // Use .default() to call the function
-      on('file:preprocessor', browserify(options));
+      
     },
   },
   component: {
--- cypress/e2e/features/petSelection.feature
+++
@@ -1,8 +0,0 @@
-Feature: Pet Selection Process
-
-  Scenario: Display PetInfo and CatHealthTracker when a pet is selected
-    Given I am on the home page
-    When I open the pet selection dropdown
-    And I select a pet
-    Then I should see the PetInfo
-    And I should see the CatHealthTracker
