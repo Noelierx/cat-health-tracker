a6c2d683df2a95e47a5534cd93194f999c7e3b92
Noëlie <noelieroux11@gmail.com>
2024-10-25 07:59:42 +0000

    pushtest

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Test</h1>
       <PetSelector onPetSelect={handlePetSelect} />
       {selectedPet && (
         <>
